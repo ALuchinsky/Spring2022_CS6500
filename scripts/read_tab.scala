a347b107d0f03a50f53f4814f8d6b1d4e6f31fce
Aleksei Luchinskii <aluchi@bgsu.edu>
2022-03-18 11:51:22 -0400

    read_table.scala commented

--- scripts/read_tab.scala
+++ scripts/read_tab.scala
@@ -1,10 +1,22 @@
 
 {
  import java.nio.file.Paths;
+
+ // Reading JSON file to a DataFrame. Note that I do not specify the schema of the file
  val path = Paths.get("data/tab.json").toAbsolutePath().toString();
  val tab = spark.read.option("multiLine", true).option("mode", "PERMISSIVE").json("file://"+path)
- val rows = tab.filter( tab("x")>0).select("str").collect()
- val words = rows.map( s => s(0).toString().split(" ")).flatMap(_.toList)
+
+// Filter rows by x and extract the str column
+ val rows = tab
+    .filter( tab("x")>0)
+    .select("str")
+    .collect()
+
+ // Split all strs  into words and flatten the Array
+ val words = rows.map( s => s(0).toString().split(" "))
+    .flatMap(_.toList)
+
+ // printing the result
  print("["+words.mkString(",")+"]")
 }
  
